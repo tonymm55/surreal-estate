12fa482a9a3e3e95ea582192e17975ee14414b5c
TonyMM55 <tonymmoran@gmail.com>
2023-04-08 11:15:30 +0100

    completed project setup with changes to eslintrc.json

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import '../styles/app.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src= className="" alt="" /> */}
+        <h2>
+          Surreal Estate
+        </h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
