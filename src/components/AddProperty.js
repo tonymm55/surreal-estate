53057c655d0eb896b69b97e6aeb9a0a113f4a425
TonyMM55 <tonymmoran@gmail.com>
2023-04-11 21:01:06 +0100

    Rendering AddProperty to the browser and logging fields object

--- src/components/AddProperty.js
+++ src/components/AddProperty.js
@@ -2,10 +2,17 @@ import React, { useState } from "react";
 import "../styles/add-property.css";
 
 const AddProperty = () => {
+  console.log("AddProperty component rendered"); // Added console log here
+
   const initialState = {
     fields: {
-      title: "",
+      title: "2 bed flat",
+      type: "Flat",
+      bedrooms: "2",
+      bathrooms: "1",
+      price: "1000",
       city: "Manchester",
+      email: "joe@mcr.codes",
     },
   };
 
@@ -28,11 +35,13 @@ const AddProperty = () => {
           <input
             id="title"
             name="title"
+            placeholder="e.g. 2 bed flat in Manchester"
             value={fields.title}
             onChange={handleFieldChange}
           />
         </label>
         <label htmlFor="city">
+          City
           <select
             id="city"
             name="city"
@@ -41,7 +50,59 @@ const AddProperty = () => {
           >
             <option value="Manchester">Manchester</option>
             <option value="Leeds">Leeds</option>
+            <option value="Sheffield">Sheffield</option>
+            <option value="Liverpool">Liverpool</option>
           </select>
+        </label>
+        <label htmlFor="type">
+          Type
+          <select
+            id="type"
+            name="type"
+            value={fields.type}
+            onChange={handleFieldChange}
+          >
+            <option value="Flat">Flat</option>
+            <option value="Detached">Detached</option>
+            <option value="Semi-Detached">Semi-Detached</option>
+            <option value="Terraced">Terraced</option>
+            <option value="End of Terrace">End of Terrace</option>
+            <option value="Cottage">Cottage</option>
+            <option value="Bungalow">Bungalow</option>
+          </select>
+        </label>
+        <label htmlFor="bedrooms">
+          Bedrooms
+          <input
+            id="bedrooms"
+            name="bedrooms"
+            type="number"
+            placeholder="e.g. 2"
+            value={fields.bedrooms}
+            onChange={handleFieldChange}
+          />
+        </label>
+        <label htmlFor="bathrooms">
+          Bathrooms
+          <input
+            id="bathrooms"
+            name="bathrooms"
+            type="number"
+            placeholder="e.g. 1"
+            value={fields.bathrooms}
+            onChange={handleFieldChange}
+          />
+        </label>
+        <label htmlFor="price">
+          Price
+          <input
+            id="price"
+            name="price"
+            type="number"
+            placeholder="e.g. 1000"
+            value={fields.price}
+            onChange={handleFieldChange}
+          />
         </label>
         <button type="submit">Add</button>
       </form>
--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import "../styles/app.css";
+import "../styles/add-property.css";
 import NavBar from "./NavBar";
 import Properties from "./Properties";
 import AddProperty from "./AddProperty";
@@ -10,12 +11,14 @@ const App = () => {
   return (
     <div className="app">
       <NavBar />
+      <AddProperty />
+      <Properties />
       <Routes>
         <Route path="/" element={Properties} />
         <Route path="add-property" element={AddProperty} />
       </Routes>
       <header className="app-header">
-        <h2>Surreal Estate</h2>
+        <h2>Header 2 Placeholder</h2>
       </header>
     </div>
   );
