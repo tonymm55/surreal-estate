4a612ce9e2ec0c04d800e7a608d6dfce2a2b8421
TonyMM55 <tonymmoran@gmail.com>
2023-04-23 16:26:50 +0100

    styling property cards

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,22 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../styles/properties.css";
 
 const SideBar = () => {
   return (
-    <div>
+    <div className="side-bar">
       <h3>Filter by City:</h3>
       <ul>
         <li>
           <Link to={`/?query={"city": "Manchester"}`}>Manchester</Link>
         </li>
         <li>
-          <Link to='/?query={"city":"Liverpool"}'>Liverpool</Link>
+          <Link to={`/?query={"city":"Liverpool"}`}>Liverpool</Link>
         </li>
         <li>
-          <Link to='/?query={"city":"Sheffield"}'>Sheffield</Link>
+          <Link to={`/?query={"city":"Sheffield"}`}>Sheffield</Link>
         </li>
         <li>
-          <Link to='/?query={"city":"Leeds"}'>Leeds</Link>
+          <Link to={`/?query={"city":"Leeds"}`}>Leeds</Link>
         </li>
       </ul>
     </div>
